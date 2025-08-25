c7ee535acc33752f0092b969d971101153ed0f5c
Andrew Borstein <andrew@andrewborstein.com>
2025-08-24 22:48:11 -0400

    Fix gofundme

--- src/components/GoFundMeEmbed.tsx
+++ src/components/GoFundMeEmbed.tsx
@@ -11,25 +11,51 @@ export default function GoFundMeIframe({ className }: GoFundMeIframeProps) {
   const src = `https://www.gofundme.com/f/paul-bedrosian/widget/medium`;
 
   return (
-    <div
-      className="bg-white shadow-md rounded-md"
-      style={{
-        width: 478,
-        minHeight: 174,
-      }}
-    >
-      <iframe
-        src={src}
-        title="'GoFundMe donation widget'"
-        loading="lazy"
+    <>
+      <div
+        className="bg-white shadow-md rounded-md block md:hidden"
+        style={{
+          width: '100%',
+          height: 223,
+          maxWidth: '100%',
+        }}
+      >
+        <iframe
+          src={src}
+          title="'GoFundMe donation widget'"
+          loading="lazy"
+          style={{
+            width: '100%',
+            border: 0,
+            minHeight: 223,
+            maxWidth: '100%',
+          }}
+          className={className}
+          allow="clipboard-write"
+        />
+      </div>
+      <div
+        className="bg-white shadow-md rounded-md hidden md:block"
         style={{
           width: 478,
-          border: 0,
-          minHeight: 174,
+          height: 173,
+          maxWidth: 478,
         }}
-        className={className}
-        allow="clipboard-write"
-      />
-    </div>
+      >
+        <iframe
+          src={src}
+          title="'GoFundMe donation widget'"
+          loading="lazy"
+          style={{
+            width: 478,
+            border: 0,
+            minHeight: 173,
+            maxWidth: '100%',
+          }}
+          className={className}
+          allow="clipboard-write"
+        />
+      </div>
+    </>
   );
 }
