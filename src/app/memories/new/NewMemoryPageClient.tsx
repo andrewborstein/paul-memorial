39fe34fd08822e39764ba55969d07af7f2cfca2e
Andrew Borstein <andrew@andrewborstein.com>
2025-08-25 10:24:20 -0400

    Update new memory page description

--- src/app/memories/new/NewMemoryPageClient.tsx
+++ src/app/memories/new/NewMemoryPageClient.tsx
@@ -35,18 +35,58 @@ export default function NewMemoryPageClient() {
                 What will you remember about Paul?
               </p>{' '}
               <p>
-                The most meaningful contributions are often the most
-                specific—not necessarily the most impressive or entertaining. A
-                conversation you had, an activity you shared, something you
-                witnessed, a message you received, or any small moment that
-                revealed who he was.
-              </p>
-              <p>
                 We each had a unique window into his life. Whether funny,
                 heartfelt, or ordinary, your memory of Paul matters. By sharing,
                 you help weave a living tribute that will comfort and inspire
                 his family and give Ólì a richer sense of the father he never
                 got to know.
+              </p>
+              <p>
+                <details>
+                  <summary>
+                    <span className="font-semibold underline text-[#184a86]">
+                      Suggestions for writing a good memory
+                    </span>
+                  </summary>
+                  <ul className="list py-4 space-y-2">
+                    <li>
+                      <span className="font-semibold">
+                        Tell us how you know Paul.
+                      </span>{' '}
+                      E.g. how you met and how long you've known each other. It
+                      will be helpful to readers in the future, including Óli.
+                    </li>
+                    <li>
+                      <span className="font-semibold">
+                        Include specific details.
+                      </span>{' '}
+                      A conversation you had, an activity you shared, something
+                      you witnessed, a message you received — any small moment
+                      that revealed who he was.
+                    </li>
+                    <li>
+                      <span className="font-semibold">
+                        It doesn't need to be exhaustive.
+                      </span>{' '}
+                      Feel free to write one very short or very long memory, or
+                      make it a series of memories.
+                    </li>
+                    <li>
+                      <span className="font-semibold">
+                        Don't worry about making it perfect.
+                      </span>{' '}
+                      You can edit or delete it after you publish, or come back
+                      and contribute more over time.
+                    </li>
+                    <li>
+                      <span className="font-semibold">
+                        Photo albums are great, too.
+                      </span>{' '}
+                      Add photos to your memory or create a separate "album"
+                      memory with a title and description of the photos.
+                    </li>
+                  </ul>
+                </details>
               </p>
             </div>
           }
